1e4e45c6fe214d4a14294419ae9f48be6638ae35
김성준 <bandalgomim@naver.com>
2024-07-14 03:37:37 +0900

    Setting: 작업환경 세팅(ver.1)

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylistic from "./eslint.formatting.mjs";
+
+export default [
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	stylistic
+]
---
+++ test/main.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from "@jest/globals";
+
+describe("Test Category Name", () => {
+    test("Test Name and Description", () => {
+        expect(1).toBe(1);
+    });
+    test("Test 2", () => {
+        expect("1").toBe(1);
+    });
+});

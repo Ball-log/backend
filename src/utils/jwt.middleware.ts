4573838bd6fef7a6dd45ba05362c718356150271
bandalgomim <bandalgomim@naver.com>
2024-07-19 02:25:01 +0900

    Fix: jwt 미들웨어 수정(Ver.1)

--- src/utils/jwt.middleware.ts
+++ src/utils/jwt.middleware.ts
@@ -4,12 +4,13 @@ import { status } from "../../config/response.status";
 
 export const authAccessTokenMiddleware = (req: Request, res: Response, next: NextFunction) => {
     if (req.headers.authorization) {
-        const token = req.headers.authorization.split("Bearer ")[1]; // header에서 access token을 가져옵니다.
+        const accessToken = req.headers.accesstoken as string;
+        const token = accessToken.split(" ")[1]; // header에서 access token을 가져옵니다.
         const result = verify(token); // token을 검증합니다.
         if (result.ok) { // token이 검증되었으면 req에 값을 세팅하고, 다음 콜백함수로 갑니다.
             next();
         } else { // 검증에 실패하거나 토큰이 만료되었다면 클라이언트에게 메세지를 담아서 응답합니다.
-            res.status(401).send(status.ACCESS_TOKEN_EXPIRED);
+            res.status(401).send(status.ACCESS_TOKEN_EXPIRED.body);
         }
     }
 };

27f2eeefe4ba4430ce3262220a5dfa7367cf3d09
Rubytubi <seongmin001015@naver.com>
2024-08-05 16:21:03 +0900

    blog 좋아요 토글, 댓글, 대댓글, 게시물 삭제 기능 추가

--- config/response.status.ts
+++ config/response.status.ts
@@ -15,7 +15,14 @@ export type ResponseType =
   | "THERE_IS_NO_TOKEN"
   | "UNKNOWN_ERROR"
   | "POST_TYPE_EMPTY"
-  | "FORBIDDEN";
+  | "FORBIDDEN"
+  | "WRONG_POST_ID"
+  | "THERE_IS_NO_POSTID_OR_BODY"
+  | "THERE_IS_NO_POSTID_OR_COMMENTID_OR_BODY"
+  | "THERE_IS_NO_TITLE_OR_CONTENT_IN_POST"
+  | "TEAM_TYPE_ERROR"
+  | "WRONG_BODY"
+  | "ONLY_AUTHOR_CAN_DELETE_OR_EDIT";
 
 export type ResponseWithStatus = {
   status: StatusCodes;
@@ -90,6 +97,58 @@ export const status: Record<ResponseType, ResponseWithStatus> = {
   POST_TYPE_EMPTY: {
     status: StatusCodes.BAD_REQUEST,
     body: { isSuccess: false, code: "400", message: "type이 비어있습니다." },
+  },
+  WRONG_POST_ID: {
+    status: StatusCodes.BAD_REQUEST,
+    body: { isSuccess: false, code: "400", message: "잘못된 post id 입니다." },
+  },
+  THERE_IS_NO_POSTID_OR_BODY: {
+    status: StatusCodes.BAD_REQUEST,
+    body: {
+      isSuccess: false,
+      code: "400",
+      message: "postId와 body는 필수 입니다.",
+    },
+  },
+  THERE_IS_NO_POSTID_OR_COMMENTID_OR_BODY: {
+    status: StatusCodes.BAD_REQUEST,
+    body: {
+      isSuccess: false,
+      code: "400",
+      message: "postId와 commentId와 body는 필수 입니다.",
+    },
+  },
+  THERE_IS_NO_TITLE_OR_CONTENT_IN_POST: {
+    status: StatusCodes.BAD_REQUEST,
+    body: {
+      isSuccess: false,
+      code: "400",
+      message: "title과 content는 빈 값일 수 없습니다.",
+    },
+  },
+  TEAM_TYPE_ERROR: {
+    status: StatusCodes.BAD_REQUEST,
+    body: {
+      isSuccess: false,
+      code: "400",
+      message: "type은 team 또는 league 중 한 값이어야 합니다.",
+    },
+  },
+  WRONG_BODY: {
+    status: StatusCodes.BAD_REQUEST,
+    body: {
+      isSuccess: false,
+      code: "400",
+      message: "body를 확인해주세요.",
+    },
+  },
+  ONLY_AUTHOR_CAN_DELETE_OR_EDIT: {
+    status: StatusCodes.BAD_REQUEST,
+    body: {
+      isSuccess: false,
+      code: "400",
+      message: "작성자만 수정 혹은 삭제를 할 수 있습니다.",
+    },
   },
   FORBIDDEN: {
     status: StatusCodes.FORBIDDEN,

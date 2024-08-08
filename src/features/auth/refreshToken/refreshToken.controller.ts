e4967fbf336bb672bde05380dc72202a62c1561b
bandalgomim <bandalgomim@naver.com>
2024-08-09 05:37:51 +0900

    Feat: CI/CD 세팅 완료

--- src/features/auth/refreshToken/refreshToken.controller.ts
+++ src/features/auth/refreshToken/refreshToken.controller.ts
@@ -3,7 +3,7 @@ import { postRefreshTokenService } from "./refreshToken.service";
 import { postRefreshTokenReqHeadersDto as reqHeaders } from "../../../models/auth/refreshToken/refreshToken.dto";
 
 export const postRefreshTokenController = async (req: Request, res: Response) => {
-    console.log(req.headers)
+    console.log(req.headers);
     const headers: reqHeaders = {
         accessToken: req.headers.authorization as string,
         refreshToken: req.headers.refreshtoken as string

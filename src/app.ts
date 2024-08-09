d700ad5f4612999d3269aed1ab03fb0c152b2084
bandalgomim <bandalgomim@naver.com>
2024-08-09 16:50:37 +0900

    희망이 보인다

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,14 @@ import express from "express";
 import { healthRoute } from "./health.route";
 import cors from "cors";
 import asyncHandler from "express-async-handler";
-
+import { config } from "dotenv";
 
 
 import { specs } from "../config/swagger.config";
 import SwaggerUi from "swagger-ui-express";
+
 /*
-import { config } from "dotenv";
+
 import { json, urlencoded } from "express"
 import { authRouter } from "./routes/auth/auth.routes";
 import { api_utilsRouter } from "./routes/api-util/api-util.route";
@@ -16,21 +17,22 @@ import { communityRouter } from "./routes/community/community.routes";
 import { myPageRouter } from "./routes/myPage/myPage.route";
 
 import { authAccessTokenMiddleware } from "./utils/jwt.middleware";
-config();
+
 */
 
+config();
 
 export default function App() {
     const app = express();
     app.use(cors());                            // cors 방식 허용
-    app.use(express.static('public'));          // 정적 파일 접근
+    app.use(express.static("public"));          // 정적 파일 접근
     app.use(express.json());                    // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
-    app.use(express.urlencoded({extended: false})); 
-    
-    app.get('/', (req, res, next) => {
-        res.send("루트 페이지!")
-    })
-    
+    app.use(express.urlencoded({ extended: false }));
+
+    app.get("/", (req, res) => {
+        res.send(process.env.PORT);
+    });
+
     app.get("/auth/login", (req, res) => {
         res.send(`
         <h1>Log in</h1>
@@ -48,12 +50,13 @@ export default function App() {
         `);
     });
 
-    app.use('/health', healthRoute)
+    app.use("/health", healthRoute);
 
-    
+
     // swagger
     app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(specs));
-/*
+
+    /*
     app.use("/auth", asyncHandler(authRouter));
     app.use(
         "/myPage",
@@ -64,7 +67,6 @@ export default function App() {
     app.use("/api-utils", asyncHandler(authAccessTokenMiddleware), api_utilsRouter);
     */
 
-    
-    
+
     return app;
 }

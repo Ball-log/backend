9b1bfd94e2931c45570cae5634b29e6614efe2f6
bandalgomim <bandalgomim@naver.com>
2024-08-09 14:40:24 +0900

    gk..

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,7 @@ config();
 initPool();
 
 const app = App();
-<<<<<<< HEAD
 
-app.listen(3001, () => {
-=======
 app.listen(3000, () => {
->>>>>>> db83157f055ddb799377da0a3f4366fd96282d8c
     console.log("Server is running on port 3000 test");
 });

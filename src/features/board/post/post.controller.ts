3a5aee02d77b400eed995f7afb536d33eac5cef9
bandalgomim <bandalgomim@naver.com>
2024-08-11 20:06:58 +0900

    Feat: 조회

--- src/features/board/post/post.controller.ts
+++ src/features/board/post/post.controller.ts
@@ -3,6 +3,7 @@ import { postService } from "./post.service";
 
 export const postController =  {
     post: async (req: Request, res: Response) => {
+        console.log(req.body)
         const user_id = res.locals.id;
         let result;
         if (req.body.type === "blog") {
@@ -13,9 +14,12 @@ export const postController =  {
         res.json(result);
     },
 
-    /* get: async (req: Request, res: Response) => {
-        // get
-    },*/
+    get: async (req: Request, res: Response) => {
+        const user_id = res.locals.id;
+        const post_id = parseInt(req.params.post_id);
+        const result = await postService.getType(post_id, user_id);
+        res.json(result);
+    },
     patch: async (req: Request, res: Response) => {
         const post_id = parseInt(req.params.post_id);
         const user_id = res.locals.id;
--- src/features/board/post/post.service.ts
+++ src/features/board/post/post.service.ts
@@ -1,13 +1,51 @@
 import { BaseApiResponse } from "../../../../config/response";
 import { status } from "../../../../config/response.status";
 import { postDao } from "../../../models/board/post/post.dao";
-import { mvpDto, blogDto } from "../../../models/board/post/post.dto";
+import { mvpDto, blogDto, getPostDto, CommentList, ReplyList, matchDto, getPostDto_ } from "../../../models/board/post/post.dto";
 
 export const postService = {
+    getType: async (post_id: number, user_id: string) => {
+        const type = await postDao.getType(post_id);
+        let result;
+        if (type === "blog") {
+            result = await postDao.getBlog(post_id, user_id);
+            console.log(result);
+        } else {
+            result = await postDao.getMvp(post_id, user_id);
+            console.log(result);
+        }
+        const res0 = result[0] as getPostDto_;
+        const res1 = result[1] as { img_url: string };
+        const res2 = result[2] as CommentList;
+        const res3 = result[3] as ReplyList;
+        const res4 = result[4] as { like_count: number };
+        const res5 = result[5] as { ex: number };
+        const match_id = res0.match_id;
+        const matchInfo = await postDao.getMatch(match_id) as matchDto;
+        const imgUrl = JSON.parse(res1.img_url);
+        const body: BaseApiResponse<getPostDto> = {
+            ...status.SUCCESS.body,
+            result: {
+                ...res0,
+                img_urls: imgUrl.img_urls as string[],
+                match_info: matchInfo,
+                comment_list: [
+                    ...res2
+                ],
+                reply_list: [
+                    ...res3
+                ],
+                like_count: res4.like_count,
+                has_liked: Boolean(res5.ex)
+            }
+        };
+        return body;
+    },
     postBlog: async (req: blogDto, user_id: string) => {
+        console.log(req)
         const result = await postDao.postBlog(req, user_id);
         const imgArr = { imgUrls: req.imgUrls };
-        const imgInfoArr = JSON.stringify(imgArr)
+        const imgInfoArr = JSON.stringify(imgArr);
         await postDao.postImg(imgInfoArr, result, req.type);
         const body: BaseApiResponse<number> = {
             ...status.SUCCESS.body,
@@ -18,7 +56,7 @@ export const postService = {
     postMvp: async (req: mvpDto, user_id: string) => {
         const result = await postDao.postMvp(req, user_id);
         const imgArr = { imgUrls: req.imgUrls };
-        const imgInfoArr = JSON.stringify(imgArr)
+        const imgInfoArr = JSON.stringify(imgArr);
         await postDao.postImg(imgInfoArr, result, req.type);
         const body: BaseApiResponse<number> = {
             ...status.SUCCESS.body,

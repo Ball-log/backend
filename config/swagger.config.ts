e0d84176901f955afc5a5e6ba5ea7e5ab0b5192c
bandalgomim <bandalgomim@naver.com>
2024-08-12 16:09:59 +0900

    Fix: 스웨거 수정

--- config/swagger.config.ts
+++ config/swagger.config.ts
@@ -8,7 +8,7 @@ const options = {
       description: "Ballog API v1",
     },
     basePath: "/",  // basepath를 basePath로 수정
-    schemes: ["http", "https"],
+    schemes: ["https" ,"http" ],
     securityDefinitions: {
       Authorization: {
         type: 'apiKey',

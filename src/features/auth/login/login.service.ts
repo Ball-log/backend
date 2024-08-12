bae57814a947da8912e3ebc06762a1cd583b1bc6
bandalgomim <bandalgomim@naver.com>
2024-08-12 17:46:47 +0900

    Fix: 댓글, 대댓글, 좋아요 라우팅 문제 해결

--- src/features/auth/login/login.service.ts
+++ src/features/auth/login/login.service.ts
@@ -4,11 +4,11 @@ import { ApiError } from "../../../../config/error";
 import { getLoginDao } from "../../../models/auth/login/login.dao";
 
 
-export const getLoginService = async (req: string) => {
-    const success = await getLoginDao(req);
+export const getLoginService = async (user_id: string) => {
+    const success = await getLoginDao(user_id);
     if (success) {
 
-        const [ accessToken, refreshToken ] = await login(req);
+        const [ accessToken, refreshToken ] = await login(user_id);
         return [ accessToken, refreshToken ];
     } else {
         throw new ApiError(status.LOGIN_INFO_UNMATCHED);

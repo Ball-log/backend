c13771057e007423641e567ecf0f0ddfb53e3e44
bandalgomim <bandalgomim@naver.com>
2024-08-30 20:02:14 +0900

    Fix: 회원가입

--- src/features/auth/signUp/signUp.controller.ts
+++ src/features/auth/signUp/signUp.controller.ts
@@ -5,9 +5,10 @@ import { getSignUpLocalsDto } from "../../../models/auth/signUp/signUp.dto";
 export const getSignUpController = async (req: Request, res: Response) => {
 
     const result = await getSignUpService(res.locals as getSignUpLocalsDto);
-    res.set("Authorization", `Bearer ${result[0]}`);
-    res.set("RefreshToken", result[1]);
-    res.send("ok");
+    res.setHeader("Authorization", "Bearer " + result[0]); // 액세스 토큰을 헤더에 설정
+    res.setHeader("RefreshToken", result[1]); // 리프레시 토큰을 헤더에 설정
+    res.setHeader("user_id", res.locals.id); //
+    res.send("Tokens sent in headers");
 };
 
 
